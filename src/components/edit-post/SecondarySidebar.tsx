82febfff384d6ce477dda5f59f0771798287b0da
Kohhee Peace <kohheepeacedev@gmail.com>
2023-12-15 03:23:05 +0900

    Confirm this version works.

--- src/components/edit-post/SecondarySidebar.tsx
+++ src/components/edit-post/SecondarySidebar.tsx
@@ -5,72 +5,72 @@
 /**
  * External dependencies
  */
-import { useEffect, useRef } from 'react';
-import { useAtom } from 'jotai';
+import { useEffect, useRef } from "react";
+import { useAtom } from "jotai";
 
 /**
  * WordPress dependencies
  */
-import { __experimentalLibrary as Library } from '@wordpress/block-editor';
-import { __experimentalUseDialog as useDialog } from '@wordpress/compose';
+import { __experimentalLibrary as Library } from "@wordpress/block-editor";
+import { __experimentalUseDialog as useDialog } from "@wordpress/compose";
 
 /**
  * Internal dependencies
  */
-import { isInserterOpenedAtom } from './atoms';
-import './SecondarySidebar.scss';
+import { isInserterOpenedAtom } from "./atoms";
+import "./SecondarySidebar.scss";
 
 export default function SecondarySidebar() {
-  const showMostUsedBlocks = false;
+	const showMostUsedBlocks = false;
 
-  /**
-   * Refs
-   */
-  const libraryRef = useRef<any | undefined>(); // Add type  }, []);
-  useEffect(() => {
-    if (libraryRef.current) {
-      libraryRef.current.focusSearch();
-    }
-  }, []);
+	/**
+	 * Refs
+	 */
+	const libraryRef = useRef();
+	useEffect(() => {
+		if (libraryRef.current) {
+			libraryRef.current.focusSearch();
+		}
+	}, []);
 
-  /**
-   * Atoms
-   */
-  const [isInserterOpened, setIsInserterOpened] = useAtom(isInserterOpenedAtom);
+	/**
+	 * Atoms
+	 */
+	const [isInserterOpened, setIsInserterOpened] = useAtom(isInserterOpenedAtom);
 
-  // https://github.com/WordPress/gutenberg/pull/27643/files
-  const [inserterDialogRef, inserterDialogProps] = useDialog({
-    onClose: () => {
-      setIsInserterOpened(false);
-    },
-    focusOnMount: null,
-  });
+	// https://github.com/WordPress/gutenberg/pull/27643/files
+	const [inserterDialogRef, inserterDialogProps] = useDialog({
+		onClose: () => {
+			setIsInserterOpened(false);
+		},
+		focusOnMount: null,
+	});
 
-  const EMPTY_INSERTION_POINT = {
-    rootClientId: undefined,
-    insertionIndex: undefined,
-  };
+	const EMPTY_INSERTION_POINT = {
+		rootClientId: undefined,
+		insertionIndex: undefined,
+	};
 
-  if (isInserterOpened) {
-    return (
-      <div
-        ref={inserterDialogRef}
-        {...inserterDialogProps}
-        className="edit-post-editor__inserter-panel"
-      >
-        <div className="edit-post-editor__inserter-panel-content">
-          <Library
-            showMostUsedBlocks={showMostUsedBlocks}
-            showInserterHelpPanel
-            shouldFocusBlock={false}
-            rootClientId={EMPTY_INSERTION_POINT.rootClientId}
-            __experimentalInsertionIndex={EMPTY_INSERTION_POINT.insertionIndex}
-            ref={libraryRef}
-          />
-        </div>
-      </div>
-    );
-  } else {
-    return null;
-  }
+	if (isInserterOpened) {
+		return (
+			<div
+				ref={inserterDialogRef}
+				{...inserterDialogProps}
+				className="edit-post-editor__inserter-panel"
+			>
+				<div className="edit-post-editor__inserter-panel-content">
+					<Library
+						showMostUsedBlocks={showMostUsedBlocks}
+						showInserterHelpPanel
+						shouldFocusBlock={false}
+						rootClientId={EMPTY_INSERTION_POINT.rootClientId}
+						__experimentalInsertionIndex={EMPTY_INSERTION_POINT.insertionIndex}
+						ref={libraryRef}
+					/>
+				</div>
+			</div>
+		);
+	} else {
+		return null;
+	}
 }
